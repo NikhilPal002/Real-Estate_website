3198249b0b7183f1c552b70f55006b1868dd4b1a
Nikhil <nikhilpal2017@gmail.com>
2023-11-10 17:08:41 +0530

    Client initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signout from './pages/SignUp';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signout />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
