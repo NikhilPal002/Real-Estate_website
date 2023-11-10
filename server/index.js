84746ff2b3eaccc6747a3c23dd8871bbaff715a5
Nikhil <nikhilpal2017@gmail.com>
2023-11-10 20:55:03 +0530

    created auth route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/authRoute.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB).then(() =>{
@@ -11,8 +12,11 @@ mongoose.connect(process.env.MONGODB).then(() =>{
 });
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server running on port 3000!');
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
