25d3f4013ba87a8c868d493e8fee141500c7d01a
Nikhil <nikhilpal2017@gmail.com>
2023-11-11 00:39:46 +0530

    created SignIn route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,19 +1,37 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const signup = async (req, res, next) =>{
-//    console.log(req.body);
-    const {username, email, password} = req.body;
+export const signup = async (req, res, next) => {
+
+    const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 12);
-    const newUser = new User({username, email, password: hashedPassword});
+    const newUser = new User({ username, email, password: hashedPassword });
     try {
         await newUser.save();
         res.status(201).json("User created succesfully!");
 
     } catch (error) {
-        // next(errorHandler(550, 'error from the function'));
         next(error);
     }
-    
+};
+
+export const signin = async (req, res, next) => {
+
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, "Invalid Credentials"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, "Invalid Credentials"));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+        res.cookie('access_token', token, { httpOnly: true, maxAge: 3600000 }).status(200).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
 };
